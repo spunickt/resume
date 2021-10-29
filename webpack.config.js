c157ee2edcce641e09c567771c255d94364a5245
spunickt <81760258+spunickt@users.noreply.github.com>
2021-10-29 10:02:01 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      // configurations for loaders will go here!
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: "body",
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
